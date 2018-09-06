6e2127c47e5b5007ddf32d13edf0513d10bd4ddc
DESKTOP-IB409D7\Sahan <sahan.randula29@gmail.com>
2018-09-06 17:39:27 +0530

    09/06 heirarchy

--- MvivoTool/Scripts/Main.js
+++ MvivoTool/Scripts/Main.js
@@ -14,7 +14,7 @@ var addingObject;
 var cubeFloor, cubeWallInt;
 var attachedComponent;
 var gridHelperGroup;
-var floorColorArray;
+var floorColorArray, wallColorArray;
 var widthBar, heightBar;
 
 window.addEventListener( 'resize', function()
@@ -42,6 +42,8 @@ window.addEventListener( 'mousedown', function(event)
 					selectedObject = intersects[0].object;
 					obj.width = selectedObject.scale.x;
 					obj.length = selectedObject.scale.z;
+					var col = selectedObject.material.color.getHex().toString(16);
+					obj.color = '#' + col;
 					gui.updateDisplay();
 					updateGui();
 				}
@@ -88,7 +90,7 @@ window.addEventListener( 'mousemove', function(event)
 				attachedComponent.position.set(Math.round(intersects[0].point.x), 0.05, Math.round(intersects[0].point.z));
 			}
 			else if(attachedComponent.componentType == "WallInt"){
-				attachedComponent.position.set((Math.round(intersects[0].point.x/0.5))*0.5, 1, (Math.round(intersects[0].point.z/0.5))*0.5);
+				attachedComponent.position.set((Math.round(intersects[0].point.x/0.5))*0.5, 1.5, (Math.round(intersects[0].point.z/0.5))*0.5);
 			}
 		}
 	}
@@ -118,6 +120,7 @@ var init = function()
 	
 	//modifying transform controls
 	transformCOntrols.setTranslationSnap(0.5);
+	transformCOntrols.setRotationSnap( THREE.Math.degToRad( 90 ) );
 	transformCOntrols.showY = false;
 	transformCOntrols.children[0].children[0].children[0].material.color = new THREE.Color( 0xffc300 );
 	transformCOntrols.children[0].children[0].children[1].material.color = new THREE.Color( 0xffc300 );
@@ -153,8 +156,8 @@ var init = function()
 	mouse = new THREE.Vector2();
 	
 	addingObject = false;
-	
-	floorColorArray = ['0xecd292','0xe5a267','0xd27254','0xa5494d','0x63223c'];
+	floorColorArray = ['0x71c7ec','0x1ebbd7','0x189ad3','0x107dac'];
+	wallColorArray = ['0xc1d11f','0x6ec007','0x34a203','0x3d860b'];
 	
 	initDatGui();
 	
@@ -168,7 +171,7 @@ var initMeshes = function()
 	var material = new THREE.MeshLambertMaterial( {color: 0xffffff, wireframe: false} );
 	cubeFloor = new THREE.Mesh( geometry, material );
 	
-	geometry = new THREE.BoxGeometry( 1, 2, 0.1);
+	geometry = new THREE.BoxGeometry( 1, 3, 0.1);
 	material = new THREE.MeshLambertMaterial( {color: 0xffffff, wireframe: false} );
 	cubeWallInt = new THREE.Mesh( geometry, material );
 };
@@ -181,6 +184,20 @@ var initDatGui = function()
 			camera.lookAt(0,0,0);
         },
 		
+		rotateB: function () {
+			transformCOntrols.setMode( "rotate" );
+			transformCOntrols.showX = false;
+			transformCOntrols.showZ = false;
+			transformCOntrols.showY = true;
+        },
+		
+		moveB: function () {
+			transformCOntrols.setMode( "translate" );
+			transformCOntrols.showX = true;
+			transformCOntrols.showZ = true;
+			transformCOntrols.showY = false;
+        },
+		
 		removel: function () {
 			if(selectedObject != null){
 				group.remove( selectedObject );
@@ -191,48 +208,51 @@ var initDatGui = function()
 			}
         },
 		
+		selectItem: function () {
+			
+        },
+		
 		floor: function () {
 			transformCOntrols.visible = false;
 			transformCOntrols.enabled = false;
 			var cube = cubeFloor.clone();
 			var mat = cube.material.clone();
-			mat.color.setHex( floorColorArray[Math.floor(Math.random() * floorColorArray.length)]  );
+			mat.color.setHex('0xffffff');
+			obj.color = "#ffffff";
 			cube.material = mat;
 			cube.componentType = "Floor";
-			cube.position.set(10,0,10);
+			cube.name = "Floor " + group.children.length;
 			group.add(cube);
 			cube.position.y = 0.05;
 			attachedComponent = cube;
 			addingObject = true;
+			highFloorFolder.add(obj, "selectItem").name(cube.name).onChange(function(){
+				transformCOntrols.attach(cube);
+				selectedObject = cube;
+			});
+			highUI.updateDisplay();
+			gui.updateDisplay();
         },
 		
-		internalWallVertical: function () {
+		internalWall: function () {
 			transformCOntrols.visible = false;
 			transformCOntrols.enabled = false;
 			var cube = cubeWallInt.clone();
 			var mat = cube.material.clone();
-			mat.color.setHex( floorColorArray[Math.floor(Math.random() * floorColorArray.length)]  );
+			mat.color.setHex('0xffffff');
+			obj.color = "#ffffff";
 			cube.material = mat;
 			cube.componentType = "WallInt";
+			cube.name = "Wall " + group.children.length;
 			group.add(cube);
 			cube.position.y = 1;
 			attachedComponent = cube;
 			addingObject = true;
-        },
-		
-		internalWallHorizontal: function () {
-			transformCOntrols.visible = false;
-			transformCOntrols.enabled = false;
-			var cube = cubeWallInt.clone();
-			var mat = cube.material.clone();
-			mat.color.setHex( floorColorArray[Math.floor(Math.random() * floorColorArray.length)]  );
-			cube.material = mat;
-			cube.componentType = "WallInt";
-			cube.rotation.y = 1.5708;
-			group.add(cube);
-			cube.position.y = 1;
-			attachedComponent = cube;
-			addingObject = true;
+			highWallFolder.add(obj, "selectItem").name(cube.name).onChange(function(){
+				transformCOntrols.attach(cube);
+				selectedObject = cube;
+			});
+			gui.updateDisplay();
         },
 		
 		externalWallLeft: function () {
@@ -241,68 +261,74 @@ var initDatGui = function()
 		
 		width: 1,
 		length: 1,
+		
+		color: "#ffffff",
     };
 	
 	gui = new dat.gui.GUI();
-
     gui.remember(obj);
-	
-	gui.add(obj, 'recenter').name('Recenter');
-	
-	gui.add(obj, 'removel').name('Remove');
-	
-	var scaleFolder = gui.addFolder('Scale');
-
-	widthBar = scaleFolder.add(obj, 'width').min(1).max(10).step(1).onFinishChange(function(){
-		selectedObject.scale.set(obj.width, 1, obj.length);
+	var scaleFolder = gui.addFolder('Properties');
+	widthBar = scaleFolder.add(obj, 'width').min(1).max(10).step(1).onChange(function(){
+		if(selectedObject != null)
+		{
+			selectedObject.scale.set(obj.width, 1, obj.length);
+		}
 	});
-	
-	heightBar = scaleFolder.add(obj, 'length').min(1).max(10).step(1).onFinishChange(function(){
-		selectedObject.scale.set(obj.width, 1, obj.length);
+	heightBar = scaleFolder.add(obj, 'length').min(1).max(10).step(1).onChange(function(){
+		if(selectedObject != null)
+		{
+			selectedObject.scale.set(obj.width, 1, obj.length);
+		}
 	});;
+	scaleFolder.addColor(obj, 'color').onChange(function(){
+		if(selectedObject != null)
+		{
+			var color = obj.color.replace("#", "x");
+			color = '0' + color;
+			selectedObject.material.color.setHex(color);
+		}
+	});;;
 	
-	var itemsFolder = gui.addFolder('House Components');
-	
+	var componentUI = new dat.gui.GUI();
+	var itemsFolder = componentUI.addFolder('House Components');
 	var floorFolder = itemsFolder.addFolder('Floors');
-	
 	floorFolder.add(obj, 'floor').name('Normal floor');
-	
 	var wallFolder = itemsFolder.addFolder('Walls');
-	wallFolder.add(obj, 'internalWallVertical').name('Vertical internal wall');
-	wallFolder.add(obj, 'internalWallHorizontal').name('Horizontal internal wall');
+	wallFolder.add(obj, 'internalWall').name('Internal wall');
 	wallFolder.add(obj, 'externalWallLeft').name('External wall left');
 	
+	var controlUI = new dat.gui.GUI();
+	var controlsFolder = controlUI.addFolder('Controls');
+	controlsFolder.add(obj, 'recenter').name('Recenter');
+	controlsFolder.add(obj, 'moveB').name('Move');
+	controlsFolder.add(obj, 'rotateB').name('Rotate');
+	controlsFolder.add(obj, 'removel').name('Remove');
+	
+	var highUI = new dat.gui.GUI();
+	var highFolder = highUI.addFolder('Hierarchy');
+	var highFloorFolder = highFolder.addFolder('Floors');
+	var highWallFolder = highFolder.addFolder('Walls');
 	scaleFolder.open();
 	itemsFolder.open();
 	floorFolder.open();
 	wallFolder.open();
+	controlsFolder.open();
+	highFolder.open();
+	highFloorFolder.open();
+	highWallFolder.open();
 };
 
 var updateGui = function()
 {
 	if(selectedObject.componentType == "WallInt")
 	{
-		if(selectedObject.rotation.y == 0)
-		{
-			heightBar.domElement.style.pointerEvents = "none"
-			heightBar.domElement.style.opacity = .5;
-			widthBar.domElement.style.pointerEvents = "auto"
-			widthBar.domElement.style.opacity = 1;
-		}
-		else
-		{
-			heightBar.domElement.style.pointerEvents = "auto"
-			heightBar.domElement.style.opacity = 1;
-			widthBar.domElement.style.pointerEvents = "none"
-			widthBar.domElement.style.opacity = .5;
-		}
+		heightBar.domElement.style.pointerEvents = "none";
+		heightBar.domElement.style.opacity = .5;
 	}
 	else
 	{
-		heightBar.domElement.style.pointerEvents = "auto"
-		heightBar.domElement.style.opacity = 1;
-		widthBar.domElement.style.pointerEvents = "auto"
-		widthBar.domElement.style.opacity =  1;
+		heightBar.domElement.style.pointerEvents = "auto";
+		heightBar.domElement.style.opacity =  1;
 	}
 };
 
